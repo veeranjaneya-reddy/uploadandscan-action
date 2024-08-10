2149a638a553de2e8f157fbb14dec803b9308879
veeranjaneya reddy <140852436+veeranjaneya-reddy@users.noreply.github.com>
2024-08-10 09:49:09 +0530

    Implemented validation of EU credentials

--- src/api/http-requests.js
+++ src/api/http-requests.js
@@ -10,15 +10,21 @@ async function getResourceByAttribute (vid, vkey, resource) {
   const queryAttribute2 = resource.queryAttribute2;
   const queryValue2 = resource.queryValue2;
   var urlQueryParams = queryAttribute !== '' ? `?${queryAttribute}=${queryValue}` : '';
+  let host = appConfig().us;
+  if (vid.startsWith('vera01ei-')) {
+    host = appConfig().eu;
+    vid = vid.split('-')[1] || '';  // Extract part after '-'
+    vkey = vkey.split('-')[1] || ''; // Extract part after '-'
+  }
   if ( queryAttribute2 ){
     urlQueryParams = urlQueryParams+`&${queryAttribute2}=${queryValue2}`;
   }
   const headers = {
-    'Authorization': calculateAuthorizationHeader(vid, vkey, appConfig().hostName, resourceUri, 
+    'Authorization': calculateAuthorizationHeader(vid, vkey, host, resourceUri, 
       urlQueryParams, 'GET')
   };
 
-  const appUrl = `https://${appConfig().hostName}${resourceUri}${urlQueryParams}`;
+  const appUrl = `https://${host}${resourceUri}${urlQueryParams}`;
   try {
     const response = await axios.get(appUrl, { headers });
     return response.data; // Access the response data
@@ -29,10 +35,16 @@ async function getResourceByAttribute (vid, vkey, resource) {
 
 async function getResource (vid, vkey, resource) {
   const resourceUri = resource.resourceUri;
+  let host = appConfig().us;
+  if (vid.startsWith('vera01ei-')) {
+    host = appConfig().eu;
+    vid = vid.split('-')[1] || '';  // Extract part after '-'
+    vkey = vkey.split('-')[1] || ''; // Extract part after '-'
+  }
   const headers = {
-    'Authorization': calculateAuthorizationHeader(vid, vkey, appConfig().hostName, resourceUri, '', 'GET')
+    'Authorization': calculateAuthorizationHeader(vid, vkey, host, resourceUri, '', 'GET')
   };
-  const appUrl = `https://${appConfig().hostName}${resourceUri}`;
+  const appUrl = `https://${host}${resourceUri}`;
   try {
     const response = await axios.get(appUrl, { headers });
     return response.data; // Access the response data
@@ -45,11 +57,11 @@ async function createResource(vid, vkey, resource) {
   const resourceUri = resource.resourceUri;
   const resourceData = resource.resourceData;
   const headers = {
-    'Authorization': calculateAuthorizationHeader(vid, vkey, appConfig().hostName, resourceUri, 
+    'Authorization': calculateAuthorizationHeader(vid, vkey, host, resourceUri, 
       '', 'POST')
   };
 
-  const appUrl = `https://${appConfig().hostName}${resourceUri}`;
+  const appUrl = `https://${host}${resourceUri}`;
   try {
     const response = await axios.post(appUrl, resourceData, { headers });
     return response.data; // Access the response data
